61f2dce0cbccca59d609ba6bb76d92cd892fb525
Jordan Fraughton <fraughtdawg@gmail.com>
2025-01-04 11:05:10 -0700

    W01: Prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool play = true; 
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 101);
+            int guess = 0;
+            int guessCount = 0;
+
+            do
+            {
+                Console.Write("What is the magic number? ");
+                string userGuessInput = Console.ReadLine();
+                guess = int.Parse(userGuessInput);
+                guessCount++;
+
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} tries!");
+                }
+            } while (guess != randomNumber);
+
+            Console.Write("Want to play again? (Y/N) ");
+            string userPlayInput = Console.ReadLine();
+            if (string.Equals(userPlayInput.ToLower(), "n"))
+            {
+                play = false;
+            }
+        } while (play == true);
     }
 }
