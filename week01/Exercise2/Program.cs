084833b1603ab98ccb20210edfb3ddfe51343ef5
Jordan Fraughton <fraughtdawg@gmail.com>
2025-01-04 10:36:02 -0700

    W01: Prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        int lastDigit = percentage % 10;
+
+        string letter;
+        string symbol = "";
+
+        if (percentage >= 90) 
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        if (lastDigit >= 7 && letter != "F" && letter != "A")
+        {
+            symbol = "+";
+        }
+        else if (lastDigit < 3 && letter != "F") 
+        {
+            symbol = "-";
+        }
+
+        Console.WriteLine($"You have a {letter}{symbol}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats. You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Womp womp. You failed.");
+        }
     }
 }
