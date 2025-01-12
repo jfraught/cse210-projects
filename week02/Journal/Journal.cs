ff7ab2937948f1c5fe739067c16d365e346879ce
Jordan Fraughton <fraughtdawg@gmail.com>
2025-01-12 12:19:08 -0700

    W02 Team Activity: Journal Design

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,26 @@
+
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        // Method implementation goes here
+    }
+
+    public void DisplayAll()
+    {
+        // Method implementation goes here
+    }
+
+    public void SaveToFile(string file)
+    {
+        // Method implementation goes here
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // Method implementation goes here
+    }
+}
