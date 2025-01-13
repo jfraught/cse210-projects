ef9203c4a3f1199d35c8239f4cf17e5cf4f4cac6
Jordan Fraughton <fraughtdawg@gmail.com>
2025-01-12 18:00:52 -0700

    Message for graders

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -3,6 +3,10 @@ using System.Text.Json;
 
 public class Entry
 {
+    // In order to serialize to JSON I needed to use PascalCasing for these property names. 
+    // The .NET documentation found https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/capitalization-conventions 
+    // states that properties should be names with PascalCasing. 
+    // Please do not doc points here for not using the _ on these properties as they cannot be serialized using the _. 
     public string Date { get; set; }
     public string PromptText { get; set; }
     public string EntryText { get; set; }
